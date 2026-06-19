50427e86db87af0b5c81681b0d9a1208159c26f4
nauxqouh <ntxhuong04@gmail.com>
2026-06-19 13:30:37 +0700

    fix: add args.normals

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -51,6 +51,7 @@ class ModelParams(ParamGroup):
         self._model_path = ""
         self._images = "images"
         self._depths = ""
+        self._normals = ""
         self._resolution = -1
         self._white_background = False
         self.data_device = "cuda"

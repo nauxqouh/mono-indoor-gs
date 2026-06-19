62fee7ea5087854a1368a06e8dc36ec5d0554621
nauxqouh <ntxhuong04@gmail.com>
2026-06-19 13:38:50 +0700

    fix: import torch

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -72,7 +72,6 @@ def loadCam(args, id, cam_info, resolution_scale):
         resolution = (int(orig_w / scale), int(orig_h / scale))
 
     if len(cam_info.image.split()) > 3:
-        import torch
         resized_image_rgb = torch.cat([PILtoTorch(im, resolution) for im in cam_info.image.split()[:3]], dim=0)
         loaded_mask = PILtoTorch(cam_info.image.split()[3], resolution)
         gt_image = resized_image_rgb
